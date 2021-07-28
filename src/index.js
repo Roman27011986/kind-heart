e09e30ab3cf230dcc693f6abcdefdd42f7616214
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-07-28 16:55:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import './index.scss';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
